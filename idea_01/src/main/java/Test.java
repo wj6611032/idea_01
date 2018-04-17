71904a0828ecd5fbfdb7c7160677804b72b79e72
wj6611032 <1066862433@qq.com>
2018-04-17 21:49:08 +0800

    ddds

--- idea_01/src/main/java/Test.java
+++ idea_01/src/main/java/Test.java
@@ -2,7 +2,9 @@ public class Test {
     @Override
     public boolean equals(Object obj) {
         System.out.println("3333");
+        System.out.println(333);
         return super.equals(obj);
+
 
     }
 }
