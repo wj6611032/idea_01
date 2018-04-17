4810bb1eae77c89d72f05a397eceac9691b6bfe7
wj6611032 <1066862433@qq.com>
2018-04-17 21:45:10 +0800

    ddds

---
+++ idea_01/src/main/java/DS.java
@@ -0,0 +1,5 @@
+public class DS {
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+}
---
+++ idea_01/src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("3333");
+        return super.equals(obj);
+
+    }
+}
